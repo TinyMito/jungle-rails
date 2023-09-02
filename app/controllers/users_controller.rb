a06424829cc3956a0390435d7b75b4e83d152ab3
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-01 22:05:28 -0400

    Add comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
+      session[:user_id] = user.id # immediately set session
       redirect_to '/'
     else
       redirect_to '/signup', notice: 'Please fill in all the fields!'
