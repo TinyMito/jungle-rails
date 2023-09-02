ba1d9512a50b1eae030574f9293aa4bba6956616
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-02 00:17:44 -0400

    Wrap in @session instead one for if user did not enter anything and one if the input is incorrect against the database.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,33 @@
 class SessionsController < ApplicationController
   def new
+    @session = Session.new #create new session
   end
   
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password]) # check user params match to db
-      session[:user_id] = user.id # set user id to session
-      redirect_to '/'
-    else # if fails
-      redirect_to '/login', notice: 'Wrong email or password, please try again!'
+    @session = Session.new(session_params) # set params
+
+    if @session.valid? # if it is not nil
+      user = User.find_by(email: @session.email) # look up User db email
+      if user && user.authenticate(session_params[:password]) # check user password hash match input
+        session[:user_id] = user.id # set user id to session
+        redirect_to '/', notice: "User #{session_params[:email]} logged in successfully!"
+      else # if fails the hash check
+        @session.errors.add(:base, 'Wrong email or password, please try again!')
+        render :new
+      end
+    else
+      render :new
     end
   end
 
   def destroy #clear session to nil
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/login', notice: "User logged out successfully!"
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password) # required email and password
   end
 end
