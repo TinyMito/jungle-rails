9c57b2ba335c8701b2849230e8f5384e350d6757
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-01 22:02:32 -0400

    Fix redirect and add error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/users'
+      redirect_to '/'
     else
-      redirect_to '/signup'
+      redirect_to '/signup', notice: 'Please fill in all the fields!'
     end
   end
 
