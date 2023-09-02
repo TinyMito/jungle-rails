b5ef3ddb74d832cfba2cd860755f3f02a2ff8ae4
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-01 22:08:18 -0400

    Update with some comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,15 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if user && user.authenticate(params[:password]) # check user params match to db
+      session[:user_id] = user.id # set user id to session
       redirect_to '/'
-    else
+    else # if fails
       redirect_to '/login', notice: 'Wrong email or password, please try again!'
     end
   end
 
-  def destroy
+  def destroy #clear session to nil
     session[:user_id] = nil
     redirect_to '/login'
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id # immediately set session
       redirect_to '/'
-    else
+    else # if signup fails
       redirect_to '/signup', notice: 'Please fill in all the fields!'
     end
   end
 
   private
 
-  def user_params
+  def user_params # santinize
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
