f0954ebe85bbd17a37239810b44153964642a0c2
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-15 23:20:49 -0400

    Add login validations

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    it 'registers successfully with good values' do
+    it 'user registers successfully with good values' do
       user = User.new(
         first_name: "FirstName", 
         last_name: "LastName", 
@@ -102,6 +102,42 @@ RSpec.describe User, type: :model do
         )
       expect(user).to_not be_valid
       expect(user.errors.full_messages).to eq ["Password is too short (minimum is 10 characters)"]
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    let(:session_params) do
+      {
+        first_name: "FirstName", 
+        last_name: "LastName", 
+        email: "test@test.com", 
+        password: 'myultimatepassword123',
+        password_confirmation: 'myultimatepassword123'
+      }
+    end
+
+    before do
+      User.create!(session_params)
+    end
+
+    it 'is able to login successfully' do
+      auth = User.authenticate_with_credentials('test@test.com','myultimatepassword123')
+      expect(auth).to be_an_instance_of(User)
+    end
+
+    it 'is not able to login with wrong password' do
+      auth = User.authenticate_with_credentials('test@test.com','wrongpassword')
+      expect(auth).to be_nil
+    end
+
+    it 'is not able to login with wrong email' do
+      auth = User.authenticate_with_credentials('test@TEST.com','myultimatepassword123')
+      expect(auth).to be_nil
+    end
+
+    it 'is not able to login with extra space in email' do
+      auth = User.authenticate_with_credentials(' test@test.com ','myultimatepassword123')
+      expect(auth).to be_nil
     end
   end
 end
