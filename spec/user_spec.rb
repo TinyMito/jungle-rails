5087e7ed63e3f5b337db6215fce6dd16d8c81579
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-15 22:38:59 -0400

    Adjust descriptions and add duplicated email test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe User, type: :model do
       expect(user.save).to be true
     end
 
-    it 'password is not the same' do
+    it 'is not valid the password is not the same' do
       user = User.new(
         first_name: "FirstName", 
         last_name: "LastName", 
@@ -25,7 +25,7 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to eq ["Password confirmation doesn't match Password"]
     end
 
-    it 'password is blank' do
+    it 'is not valid password is missing' do
       user = User.new(
         first_name: "FirstName", 
         last_name: "LastName", 
@@ -37,7 +37,7 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to eq ["Password can't be blank"]
     end
 
-    it 'first name is blank' do
+    it 'is not valid first name is missing' do
       user = User.new(
         first_name: nil, 
         last_name: "LastName", 
@@ -49,7 +49,7 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to eq ["First name can't be blank"]
     end
 
-    it 'last name is blank' do
+    it 'is not valid last name is missing' do
       user = User.new(
         first_name: "FirstName", 
         last_name: nil, 
@@ -61,7 +61,7 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to eq ["Last name can't be blank"]
     end
 
-    it 'email is blank' do
+    it 'is not valid email is missing' do
       user = User.new(
         first_name: "FirstName", 
         last_name: "LastName", 
@@ -71,6 +71,24 @@ RSpec.describe User, type: :model do
         )
       expect(user).to_not be_valid
       expect(user.errors.full_messages).to eq ["Email can't be blank"]
+    end
+
+    it 'is not valid email is duplicated' do
+      User.create!(
+        first_name: "FirstName2", 
+        last_name: "LastName2", 
+        email: "TEST@TEST.com", 
+        password: 'pass321',
+        password_confirmation: 'pass321'
+      )
+      user = User.new(
+        first_name: "FirstName1", 
+        last_name: "LastName1", 
+        email: "test@test.com", 
+        password: 'pass123',
+        password_confirmation: 'pass123'
+        )
+      expect(user).to_not be_valid
     end
   end
 end
